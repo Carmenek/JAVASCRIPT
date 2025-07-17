ae14fceaeb3c536240960b0ec0808b4173af8e3c
Ange <angecarmenek@gmail.com>
2025-07-17 19:10:04 +0530

    Loops

---
+++ console/script.js
@@ -0,0 +1,96 @@
+// Lesson 1
+
+
+// String
+// mumber
+// undefined
+// NaN (not a number)
+// Object
+// Boolean
+
+
+// "" double quotes
+// // '' single quote
+// // `` ${} string interporation
+
+// let w=prompt("enter first number")
+//     // we use prompt function to get the user input data
+// let r=prompt("enter second number")
+//     // we use prompt function to get the user input data
+
+// let nn = `la multiplication de ${(w*r)}`
+//     // the we construct the multiplication string to display for the two provided variable w an r
+// console.log(nn)
+    // and then we display the constructed result of nn
+// ============
+// 
+
+        //Ex1: Exercice make the  multiplication of two input field's data
+
+//===================================== 
+
+
+// Lesson 2 (Conditions)
+// AND = &
+// OR = |
+// true and false = true
+// false and true = true
+// false and false = false
+
+// true or false= true
+// true or true = true
+// false or true= true
+// false or false = false
+
+// let a=9
+// a==9; //values
+// a===9 // values and types
+// if(9==="9" | 6==="6")
+// {
+//     console.log("washaje");
+// }
+
+// else if(nn<90){
+//     console.log("jiskmc");
+// }
+
+// else{
+//     console.log("nothing")
+// }
+
+// ================================================
+
+// lessons (Loops)
+
+// Infinite loops
+
+// do while
+// do{
+//     var aaa=1;
+
+//     console.log(aaa);
+//     aaa++;
+// }while(aaa < 1)
+
+// // for
+
+// // while
+
+// while(aaa < 1){
+//     console.log(aaa);
+// }
+
+
+
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+
+var i = prompt("Enter your age");
+if (i >= 18) {
+    console.log("You are an adult.");
+}   
+else{
+    console.log("u're a minor.")
+}
--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// var PI=3.14;
+// // var PI=3.14;
 
-// PI=23.3;
-// console.log(PI); 
+// // PI=23.3;
+// // console.log(PI); 
 
-// PI=3.14159;
+// // PI=3.14159;
 
-// console.log(PI); // This will throw an error because PI is a constant and cannot be reassigned.
+// // console.log(PI); // This will throw an error because PI is a constant and cannot be reassigned.
 
-let myname = "Irakoze Chris"; // This is a block-scoped constant
-if(true) {
-    var myname1 = "Ange"; // This is a block-scoped constant
-    console.log(myname); // This will log 3.14
-    console.log(myname1); // This will log 3.14
-}
-console.log(myname); // This will log 3.14
-console.log(myname1); // This will log 3.
+// let myname = "Irakoze Chris"; // This is a block-scoped constant
+// if(true) {
+//     var myname1 = "Ange"; // This is a block-scoped constant
+//     console.log(myname); // This will log 3.14
+//     console.log(myname1); // This will log 3.14
+// }
+// console.log(myname); // This will log 3.14
+// console.log(myname1); // This will log 3.
  
-let y = 111111111; // This is a block-scoped variable
-let x={name:"Irakoze",premom:"Chris",height:1.77}; // This is a block-scoped variable
-console.log(typeof y ); // This will log "irakoze"
+// let y = 111111111; // This is a block-scoped variable
+// let x={name:"Irakoze",premom:"Chris",height:1.77}; // This is a block-scoped variable
+// // console.log(typeof y ); // This will log "irakoze"
